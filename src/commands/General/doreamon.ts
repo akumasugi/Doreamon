88a0d954f49d430c1f04e10beea2c06ae0b56add
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-06-04 14:12:09 +0530

    Create doreamon.ts

---
+++ src/commands/General/doreamon.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "dora",
+			description: "Displays the info",
+			category: "general",
+			usage: `${client.config.prefix}elaina`,
+			baseXp: 200,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://telegra.ph/file/3f0a577c86cc285a67134.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: ` *🐱DOREAMON🐱* \n\n🐱 *Description: repo is private better luck next time.*\n\n🐱 *insta id: https://www.instagram.com/akuma__24/?hl=en* \n\n 🐱\n`,
+			}
+		);
+	};
+}
