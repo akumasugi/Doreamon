606b867d286db1d02be000f1c9776f24bab96858
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-06-04 14:33:18 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -80,8 +80,8 @@ export default class Command extends BaseCommand {
 					})();
 					return {
 						categories,
-						pack: pack[1] || "DOREAMON⚡ ",
-						author: pack[2] || "BY KAI⚡",
+						pack: pack[1] || "𝐙𝐄𝐑𝐎❤️",
+						author: pack[2] || "𝐀𝐊𝐔𝐌𝐀",
 						quality,
 						type: StickerTypes[
 							parsedArgs.flags.includes("--crop") ||
